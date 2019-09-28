e7f233241191a56557ce25ff55a9dd047eca228c
DrXp3rT0 <shaikhtalha6604@gmail.com>
2019-09-28 16:15:31 +0530

    Commit_2: search and follow

---
+++ app/src/main/java/com/vampyr/demo/Adapter/UserAdapter.java
@@ -0,0 +1,143 @@
+package com.vampyr.demo.Adapter;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.vampyr.demo.Fragments.ProfileFragment;
+import com.vampyr.demo.Model.Users;
+import com.vampyr.demo.R;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
+
+    private Context mContext;
+    private List<Users> mUsers;
+
+    private FirebaseUser firebaseUser;
+
+    public UserAdapter(Context mContext, List<Users> mUsers) {
+        this.mContext = mContext;
+        this.mUsers = mUsers;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item,parent,false);
+
+        return new UserAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        final Users users = mUsers.get(position);
+
+        holder.followText.setVisibility(View.VISIBLE);
+        holder.username.setText(users.getUsername());
+        holder.fullname.setText(users.getFullname());
+        isFollowing(users.getId(),holder.followText);
+        Glide.with(mContext).load(users.getImageurl()).into(holder.image_Profile);
+
+        if (users.getId().equals(firebaseUser.getUid())){
+
+            holder.followText.setVisibility(View.GONE);
+        }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = mContext.getSharedPreferences("PREFS", Context.MODE_PRIVATE).edit();
+                editor.putString("profileid", users.getId());
+                editor.apply();
+
+                ((FragmentActivity)mContext).getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
+            }
+        });
+
+        holder.followText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (holder.followText.getText().toString().equals("follow")){
+                    FirebaseDatabase.getInstance().getReference().child("follow").child(firebaseUser.getUid())
+                            .child("following").child(users.getId()).setValue(true);
+                    FirebaseDatabase.getInstance().getReference().child("follow").child(users.getId())
+                            .child("followers").child(firebaseUser.getUid()).setValue(true);
+                }else {
+                    FirebaseDatabase.getInstance().getReference().child("follow").child(firebaseUser.getUid())
+                            .child("following").child(users.getId()).removeValue();
+                    FirebaseDatabase.getInstance().getReference().child("follow").child(users.getId())
+                            .child("followers").child(firebaseUser.getUid()).removeValue();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView username;
+        public TextView fullname;
+        public CircleImageView image_Profile;
+        public TextView followText;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            username = itemView.findViewById(R.id.username);
+            fullname = itemView.findViewById(R.id.fullname);
+            image_Profile = itemView.findViewById(R.id.image_profile);
+            followText = itemView.findViewById(R.id.follow_btn);
+        }
+    }
+
+
+    private void isFollowing(final String userid, final TextView button){
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference()
+                .child("Follow").child(firebaseUser.getUid()).child("following");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.child(userid).exists()){
+                    button.setText("following");
+                }else {
+                    button.setText("follow");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/vampyr/demo/Model/Users.java
@@ -0,0 +1,60 @@
+package com.vampyr.demo.Model;
+
+public class Users {
+
+    private String id;
+    private String username;
+    private String fullname;
+    private String imageurl;
+    private String bio;
+
+    public Users() {}
+
+    public Users(String id, String username, String fullname, String imageurl, String brio) {
+        this.id = id;
+        this.username = username;
+        this.fullname = fullname;
+        this.imageurl = imageurl;
+        this.bio = bio;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public void setImageurl(String imageurl) {
+        this.imageurl = imageurl;
+    }
+
+    public String getBrio() {
+        return bio;
+    }
+
+    public void setBrio(String brio) {
+        this.bio = brio;
+    }
+}
