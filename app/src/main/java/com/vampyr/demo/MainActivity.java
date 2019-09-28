72be42e610028060321a07d2e2d8bac3778dd530
DrXp3rT0 <shaikhtalha6604@gmail.com>
2019-09-28 23:20:42 +0530

    Commit_6: added navigation drawer and it works...added new colors

--- app/src/main/java/com/vampyr/demo/MainActivity.java
+++ app/src/main/java/com/vampyr/demo/MainActivity.java
@@ -1,34 +1,59 @@
 package com.vampyr.demo;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.vampyr.demo.Fragments.ChatsFragment;
 import com.vampyr.demo.Fragments.DashboardFragment;
 import com.vampyr.demo.Fragments.ProfileFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    DrawerLayout drawerLayout;
     Toolbar toolbar;
     BottomNavigationView bottomNavigationView;
     Fragment selectedFragment = null;
+
+    FirebaseAuth mAuth;
+    FirebaseUser firebaseUser;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        mAuth = FirebaseAuth.getInstance();
+        loadingBar = new ProgressDialog(this);
+
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
 
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
 
@@ -62,8 +87,37 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
     }
 
 
+    @Override
+    public void onBackPressed() {
 
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()){
+            case R.id.nav_logout:
+                loadingBar.setTitle("Signing out");
+                loadingBar.setMessage("Please wait...");
+                loadingBar.show();
+                mAuth.signOut();
+                SendUserToLoginActivity();
+        }
+
+        return true;
+    }
+
+    private void SendUserToLoginActivity() {
+        Intent loginIntent = new Intent(MainActivity.this, StartActivity.class);
+        startActivity(loginIntent);
+    }
 }
